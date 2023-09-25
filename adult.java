2731e91d2a828ff838be495a7a060d3a8efd3a0d
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-09-25 17:07:07 +0530

    Add files via upload

---
+++ adult.java
@@ -0,0 +1,14 @@
+class adult{
+public static void main(String[]args){
+
+  int sonu=19;
+
+   if(sonu>=18)
+
+     System.out.println("eligible to vote-->yess");
+   
+   else
+   
+    System.out.println("eligible to vote-->No");
+}
+}
