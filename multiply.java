f0f7375f0e684b164a295328825dc14a33120400
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-09-25 17:18:49 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,11 @@
+class multiply{
+public static void main(String[]args){
+
+  int a=5;
+  int b=5;
+  int c=a*b;
+
+ System.out.println("multiply of a & b is:"+c);
+}
+}
+ 
---
+++ negativepositive.java
@@ -0,0 +1,15 @@
+class negativepositive{
+public static void main(String[]args){
+
+  int a=-99;
+
+  if(a>0)
+  
+  System.out.println("positive");
+
+  else
+ s
+  System.out.println("negative");
+
+}
+}
