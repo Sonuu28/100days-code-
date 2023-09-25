1fa462a79932738e96a6d64f262a0ccc9d1340b5
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-09-25 17:17:21 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,37 @@
+class greater{
+public static void main(String[]args){
+
+  int a=60;
+  int b=34;
+  int c=38;
+
+  if(a>b && a>c)
+
+  System.out.println("a is greater");
+
+  else if(b>a && c>c)
+
+  System.out.println("b is greater");
+
+  else
+
+  System.out.println("c is greater");
+
+  if(a<b && a<c)
+
+  System.out.println("a is smaller");
+
+  else if(b<a && b<c)
+
+  System.out.println("b ic smaller");
+
+  else
+
+  System.out.println("c is smaller");
+
+ }
+}
+
+
+
+ 
---
+++ multipleof5.java
@@ -0,0 +1,15 @@
+class multiply{
+public static void main(String[]args){
+
+  int a=34;
+
+  if(a%5==0)
+
+   System.out.println("yess");
+  
+  else
+
+  System.out.println("noo");
+ 
+  }
+}
