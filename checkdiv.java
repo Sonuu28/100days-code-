b4750b54235cb857c4dee978446c5ac9c7bdaced
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-09-25 17:11:55 +0530

    Add files via upload

---
+++ checkdiv.java
@@ -0,0 +1,14 @@
+class check{
+public static void main(String[]args){
+
+  int a=15;
+
+  if(a%3==0 && a%7==0)
+   
+    System.out.println("yes");
+  
+   else
+    
+    System.out.println("n0");
+}
+}
