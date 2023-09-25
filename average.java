3ed3b7ab6458f9f7fa701b21ab0e5cbd5790c9cc
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-09-25 17:09:56 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,14 @@
+class average{
+public static void main(String[] args){
+
+      int a=5;
+      int b=6;
+      int c=7;
+      int d=8;
+      int e=9;
+System.out.println("average of five numbers"+(a+b+c+d+e)/5);
+}
+}
+
+
+ 
