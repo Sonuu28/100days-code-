d93e88929e01ccd4f9020d1e4e668097be6228bc
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-09-25 17:08:34 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,14 @@
+class alphabet{
+public static void main(String[]args){
+
+  char value='$';
+
+  if(value>='a' && value<='z' || value>='A' && value<='Z')
+
+  System.out.println("alphabet");
+
+  else
+
+  System.out.println("not alphabet");
+}
+}
