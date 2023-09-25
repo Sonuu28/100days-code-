09ab4b2fec200992ac48faeaca53e1836dfd17f6
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-09-25 17:14:02 +0530

    Add files via upload

---
+++ checknum.java
@@ -0,0 +1,15 @@
+class check{
+public static void main(String[]rgs){
+ 
+  int a=14;
+
+  if(a%3==0 || a%7==0)
+ 
+    System.out.println("divisible");
+
+  else
+
+     System.out.println("not divisible");
+
+ }
+}
