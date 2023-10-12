935985d8b233dcf9a4bbaa80a76bd2f4b8991eaa
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-10-12 15:39:44 +0530

    Add files via upload

---
+++ square_1ton.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class square_1ton{
+public static void main(String[]args){
+ int num,j;
+  Scanner sc = new Scanner(System.in);
+  System.out.println("please enter a number");
+  int a=sc.nextInt();
+   
+  
+   for(num=1; num<=a; num++)
+    {     
+      System.out.println("square-->"+num*num);
+    }
+  }
+}
+  
+   
