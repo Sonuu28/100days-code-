3af94f67e44e3833a61adcbd03061553ee15d402
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-10-12 14:00:50 +0530

    Add files via upload

---
+++ average_no_n.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class average_no_n{
+public static void main(String[]args){
+ 
+  Scanner sc =new Scanner(System.in);
+ System.out.println("please enter a value");
+  int n=sc.nextInt();
+  int j=n/n;
+int i;
+  for(i=1;i<=n;i++)
+    System.out.println("average-->"+j);
+}
+}
