ec4b7e9eedc910d2c2b535ef917c2e271f75512f
Sonuu28 <145605605+Sonuu28@users.noreply.github.com>
2023-10-12 14:04:24 +0530

    Add files via upload

---
+++ even_13.java
@@ -0,0 +1,22 @@
+class even_13{
+public static void main(String[]args){
+  
+  int i;
+  int n=25;
+  int sum=0;
+  for(i=0; i<=n; i++)
+   { 
+    if(i%2==0)
+      {
+        System.out.println("number is even-->"+i);
+         System.out.println("sum-->"+sum+i);
+        
+       }
+        
+     }
+   }
+} 
+        
+        
+
+  
